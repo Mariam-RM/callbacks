087d803e92dfaf273807204b53339a74df13ac96
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-11-29 16:42:35 +0000

    refactored with for-each

--- callbackFunctionsEg.js
+++ callbackFunctionsEg.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(arr,arr[i]);   // execute callback
+  arr.forEach(function(thisI){
+    if (thisI === "Waldo") {
+      found(arr,thisI)  // execute callback
     }
-  }
+  }) // for (var i = 0; i < arr.length; i++) {
 }
 
 function actionWhenFound(arr, blank2) {
