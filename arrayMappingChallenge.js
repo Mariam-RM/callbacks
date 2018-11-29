0f4381504bc2ddd835455ace1e2383276246acfb
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-11-29 17:11:15 +0000

    completed challenge

--- arrayMappingChallenge.js
+++ arrayMappingChallenge.js
@@ -1,10 +1,11 @@
-ar input = [
+var input = [
   { x: 3, y: 4 },
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
 var result = input.map(function(num){
-    return num*num;
+    return Math.sqrt(num.x*num.x + num.y*num.y);
+
   }
 );
 console.log(result[0] === 5);
