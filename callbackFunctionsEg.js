651953d1848752730656ecad61b9d37d080a154a
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-11-29 16:25:57 +0000

    modified callback to return index of waldo

--- callbackFunctionsEg.js
+++ callbackFunctionsEg.js
@@ -7,11 +7,11 @@ function findWaldo(arr, found) {
   }
 }
 
-function actionWhenFound(blank1, blank2) {
-  console.log("Found him! at index " + blank1.indexOf(blank2) + "!");
+function actionWhenFound(arr, blank2) {
+  console.log("Found him! at index " + arr.indexOf(blank2) + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound(blank1,blank2));
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound) ;
 
 
 // Modify the callback function in the previous example so that logs the index of the array where Waldo is found, ie. "Found Waldo at index 2!". (You will need to modify actionWhenFound to receive the index.)
